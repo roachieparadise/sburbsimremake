d4b199f936710b99e80874dcc181ebd8e9033dbd
roachieparadise <chest.nuts232@gmail.com>
2025-10-29 16:39:32 +0100

    Add CORS configuration

--- public/scripts/sessionhandler.js
+++
@@ -1,8 +0,0 @@
-function createContent() {
-    const container = document.getElementById('content');
-    if (!container) {
-        console.warn('it didnt WORK');
-        return;
-    }
-    container.innerHTML = `<p>testing this</p>`;
-}
